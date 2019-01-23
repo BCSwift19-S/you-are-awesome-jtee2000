64850e7bfbca3a193699a521bb8b5c4e0d1d9d2c
Jason Tee <Jason@jasons-air.bc.edu>
2019-01-23 02:53:39 -0500

    Completed 2.6

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -19,15 +19,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome"
-        messageLabel.textColor = UIColor.green
+        if messageLabel.text == "You Are Awesome!" {
+            messageLabel.text = "You Are Great!"
+        } else if messageLabel.text == "You Are Awesome!" {
+                messageLabel.text = "You Are Amazing!"
+        } else {
+            messageLabel.text = "You Are Awesome!"
+        }
   
     }
     
-    
-    @IBAction func secondButton(_ sender: UIButton) {
-        messageLabel.text = "You are Great!"
-    }
     
 }
 
