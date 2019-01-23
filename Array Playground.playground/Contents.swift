755b8934738b76394b4f9ecd7e8eae00f11f3b9c
Jason Tee <Jason@jasons-air.bc.edu>
2019-01-23 04:15:55 -0500

    Completed 2.10

---
+++ Array Playground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+str = "Hello, Developer"
+print(str)
+
+let message = "You Are Awesome!"
+let message2 = "You Are Great!"
+let message3 = "You Are Amazing!"
+
+let messages = ["You Are Awesome", "You Are Great", "You Are Amazing", "You Are Fantastic!", "You Are Da Bomb"]
+print(messages[0])
+messages.count
+messages[messages.count-1]
+messages.first
+messages.last
+
--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UITextField!
+    var index = 0
     
     //Code below executes when the app first loads
     override func viewDidLoad() {
@@ -19,17 +20,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You Are Awesome!"
-        let message2 = "You Are Great!"
-        let message3 = "You Are Amazing!"
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-                messageLabel.text = message3
-        } else {
-            messageLabel.text = message1
+        let messages = ["You Are Awesome",
+                        "You Are Great",
+                        "You Are Amazing",
+                        "You Are Fantastic!",
+                        "You Are Da Bomb"]
+        messageLabel.text = messages[index]
+        
+        if index == messages.count-1 {
+            index = 0
         }
+        index+=1
+//        let message1 = "You Are Awesome!"
+//        let message2 = "You Are Great!"
+//        let message3 = "You Are Amazing!"
+//
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//        } else if messageLabel.text == message2 {
+//                messageLabel.text = message3
+//        } else {
+//            messageLabel.text = message1
+//        }
   
     }
     
